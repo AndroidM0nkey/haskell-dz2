cfee458ee8ec0b496565545ae05ac25168eb71ae
AndroidM0nkey <supergamer170302@gmail.com>
2023-04-04 19:32:58 +0300

    more handlers

--- app/Main.hs
+++ app/Main.hs
@@ -19,27 +19,73 @@ import qualified Data.IntMap.Strict as M
 import Data.IORef
 import Control.Monad.IO.Class
 import Control.Monad.State
+import Data.Monoid (Sum(..))
 
 
 -- API type
-type UserAPI1 = "users" :> Get '[JSON] [User]
+type UserAPI1 = "new_account" :> Get '[JSON] Acc
+           :<|> "balance" :> Capture "acc" Int :> Get '[JSON] Bal
+           :<|> "deposit" :> Capture "acc" Int :> Capture "amount" (Sum Int) :> Get '[JSON] Ans
+           :<|> "withdraw" :> Capture "acc" Int :> Capture "amount" (Sum Int) :> Get '[JSON] Ans
+
+
 
 -- JSON that will be used in answer
-data User = User
+data Acc = Acc
   { account_id :: Int
   } deriving (Eq, Show, Generic)
 
-instance ToJSON User
+instance ToJSON Acc
 
-server1 ref = do
-    iostate <- liftIO $ readIORef ref
-    let s = evalStateT (runPureBank newAccountHttp) iostate in case s of
-      -- эта ручка всегда возвращает что-то валидное
-      Right s2 -> do
-        let rs = runStateT (runPureBank newAccountHttp) iostate in case rs of
-          Right rs2 -> do
-            tmp <- liftIO $ writeIORef ref (snd rs2)
-            return [User s2]
+data Bal = Bal
+  { balance :: Int
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON Bal
+
+data Ans = Ans
+  { answer :: String
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON Ans
+
+server1 ref = processNewAccount ref
+         :<|> processBalance ref
+         :<|> processDeposit ref
+         :<|> processWithdraw ref
+        where processNewAccount ref = do
+                iostate <- liftIO $ readIORef ref
+                let s = evalStateT (runPureBank newAccountHttp) iostate in case s of
+                  -- эта ручка всегда возвращает что-то валидное
+                  Right s2 -> do
+                    let rs = runStateT (runPureBank newAccountHttp) iostate in case rs of
+                      Right rs2 -> do
+                        tmp <- liftIO $ writeIORef ref (snd rs2)
+                        return $ Acc s2
+
+              processBalance ref acc = do
+                iostate <- liftIO $ readIORef ref
+                let s = evalStateT (runPureBank $ balanceHttp acc) iostate in case s of
+                  --TODO: добавить обработку ошибки
+                  Right rs -> return $ Bal (getSum rs)
+
+              processDeposit ref acc amount = do
+                iostate <- liftIO $ readIORef ref
+                let s = runStateT (runPureBank $ depositHttp acc amount) iostate in case s of
+                  --TODO: добавить обработку ошибки
+                  Right rs -> do
+                    tmp <- liftIO $ writeIORef ref (snd rs)
+                    return $ Ans "success"
+
+              processWithdraw ref acc amount = do
+                iostate <- liftIO $ readIORef ref
+                let s = runStateT (runPureBank $ withdrawHttp acc amount) iostate in case s of
+                  --TODO: добавить обработку ошибки
+                  Right rs -> do
+                    tmp <- liftIO $ writeIORef ref (snd rs)
+                    return $ Ans "success"
+
+
 
 userAPI :: Proxy UserAPI1
 userAPI = Proxy
